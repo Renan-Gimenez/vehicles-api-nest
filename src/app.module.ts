d6e373b3bafaa5c2a2b288b248851c4d4121134c
Renan Gimenez <renangiimenez@gmail.com>
2025-06-08 01:33:22 -0300

    feat: setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehiclesModule } from './vehicles/vehicles.module';
 
+import { env } from './env';
+
 @Module({
-  imports: [VehiclesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      username: env.DB_USERNAME,
+      password: env.DB_PASSWORD,
+      database: env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    VehiclesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

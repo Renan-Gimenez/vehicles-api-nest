be80424b96bfdeb5c123f92ff4ed760488aaf1cf
Renan Gimenez <renangiimenez@gmail.com>
2025-06-08 23:12:35 -0300

    feat: add health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
+import { ApiResponse } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiResponse({
+    status: 200,
+    description: 'Application is healthy',
+    example: 'OK',
+  })
+  @Get('health')
+  getHealth(): string {
+    return this.appService.getHealth();
   }
 }
